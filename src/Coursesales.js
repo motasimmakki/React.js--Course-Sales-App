c27c74f119a1beeff4d7ae907e1dc0417fcde710
Motasim <makkimotasim@gmail.com>
2020-08-21 12:28:02 +0530

    Initial Commit

--- src/Coursesales.js
+++ src/Coursesales.js
@@ -1,11 +1,60 @@
 import React,{ Component } from 'react';
 
 export default class Coursesales extends Component {
+
+  sumPrice(price){
+    this.setState({total: this.state.total+price});
+  }
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      total: 0
+    };
+    this.sumPrice = this.sumPrice.bind(this);
+  }
+
   render(){
     console.log(this.props.items);
+    var courses = this.props.items.map((item, i)=>{
+      return <Course name={item.name} price={item.price}
+        key={i} sumPrice={this.sumPrice} active={item.active}
+      />;
+    });
     return(
       <div>
-        I am from Course Sales Page. <br />
+        <h2> You can Buy Courses :</h2><br />
+        <div id="courses">
+          {courses}
+          <p><b>Total : </b>{this.state.total}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Course extends Component {
+
+  clicker(){
+    var active = !this.state.active;
+    this.setState({active:active});
+    this.props.sumPrice(active? this.props.price: -this.props.price);
+  }
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      active:false
+    };
+    this.clicker = this.clicker.bind(this);
+  }
+
+  render(){
+    return(
+      <div>
+        <p onClick={this.clicker}>{this.props.name} = <b>{this.props.price}</b></p>
       </div>
     );
   }
